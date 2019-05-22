ceba1192af1ad2fad907e498e675df01343747dc
elbosom <31boso@gmail.com>
2019-05-22 17:11:29 +0200

    add basic math operators

--- calcul.js
+++ calcul.js
@@ -1,6 +1,28 @@
-
+// 1,2,4,124,n = 1+124+n
 function add(...numbers) {
   return numbers.reduce((accumulator, currentValue)=>{
-    accumulator + currentValue;
+    return accumulator + currentValue;
   });
-}
+};
+
+// 1,2,4,124,n = 1-124-n
+function substract(...numbers) {
+  return numbers.reduce((accumulator, currentValue)=>{
+    return accumulator - currentValue;
+  });
+};
+
+function multiply(...numbers) {
+  return numbers.reduce((accumulator, currentValue)=>{
+    return accumulator * currentValue;
+  });
+};
+
+function divide(...numbers) {
+  return numbers.reduce((accumulator, currentValue)=>{
+    return accumulator / currentValue;
+  });
+};
+const operators = [plus,minus,multy,div];
+
+function operate(num1,num2)
