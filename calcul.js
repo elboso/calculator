11c922a826d61cf19dcc3502a6551aa42c06e667
elboso <31boso@gmail.com>
2019-05-26 11:30:43 +0200

    Add compute function (doesn't acount for operation order)

--- calcul.js
+++ calcul.js
@@ -1,3 +1,5 @@
+let displayInput =[24,'+',8,'-',4];
+
 // 1,2,4,124,n = 1+124+n
 function add(...numbers) {
   return numbers.reduce((accumulator, currentValue)=>{
@@ -25,4 +27,24 @@ function divide(...numbers) {
 };
 const operators = [plus,minus,multy,div];
 
-function operate(num1,num2)
+function compute(arr){
+  let tempArr = arr.map(x=>x);
+  if (tempArr.length>2 && typeof tempArr[tempArr.length-1]==='number'){
+    let rslt = 'argtttt';
+    if (tempArr[1]==='+') {
+      rslt = add(tempArr[0],tempArr[2]);
+    }
+    else if (tempArr[1]==='-') {
+      rslt = substract(tempArr[0],tempArr[2]);
+    }
+    else if (tempArr[1]==='*') {
+      rslt = multiply(arr[0],tempArr[2]);
+    }
+    else if (tempArr[1]==='/') {
+      rslt = divide(tempArr[0],tempArr[2]);
+    }
+    tempArr.splice(0,3,rslt);
+    return compute(tempArr);
+  }
+  else return tempArr[0];
+}
